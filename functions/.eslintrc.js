7a070277ebb79c63baf21d48cb441c76b50f5ace
Sandiso Mayekiso <mayekisosandiso@gmail.com>
2026-08-15 15:54:49 +0200

    Add files via upload

---
+++ functions/.eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+
+  parserOptions: {
+    "ecmaVersion": 2022,
+  },
+
+  extends: [
+    "eslint:recommended",
+    "google",
+  ],
+
+  rules: {
+    "no-restricted-globals": [
+      "error",
+      "name",
+      "length",
+    ],
+
+    "prefer-arrow-callback": "error",
+
+    "quotes": [
+      "error",
+      "double",
+      {
+        "allowTemplateLiterals": true,
+      },
+    ],
+
+    /*
+     * CWS Academy Firebase Functions use descriptive
+     * section comments rather than JSDoc for every
+     * helper function.
+     */
+    "require-jsdoc": "off",
+  },
+
+  overrides: [
+    {
+      files: [
+        "**/*.spec.*",
+      ],
+
+      env: {
+        mocha: true,
+      },
+
+      rules: {},
+    },
+  ],
+
+  globals: {},
+};
